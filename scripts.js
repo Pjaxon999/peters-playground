bb3e3cbc2784bfbf7b278d3042d8157a8b93be90
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-17 10:13:06 -0700

    Complete funcitonality of button sizing "normally"
    This is how I would normally make something like this before trying to
    utilize any of the content from the TOP lesson. I'll see if I can get
    things working utilizing closures and factory functions before later on
    trying to wrap the whole thing into a module using IIFE.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // test external link
 console.log("Hello there!");
+
+// Basic idea here is to explore the scope, closure, factory function, IIFE stuff I've been trying to learn
+// We have a set of buttons which will change the font-size style property to whatever the textContent of the
+// button is. I'll try to write it in different ways utilizing these concepts
+
+// Grab buttons
+const btns = document.querySelectorAll(".btn");
+btns.forEach(button => {
+    button.addEventListener("click", fontSizer)        
+});
+
+function fontSizer(e){
+    let newSize = parseInt(e.target.textContent);
+    document.body.style.fontSize = `${newSize}px`;
+}
