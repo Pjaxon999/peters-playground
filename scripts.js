ae23d39dc4e48364be203e38fedccf29902bf557
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-17 10:49:38 -0700

    Refactored to keep btns const "private"
    I guess this utilizes the fact that btns is now no longer polluting the
    global scope so that is something, but I think in order to utilize
    a closure I am going to need to temporarily pollute the namespace so
    that is fun.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,16 @@ console.log("Hello there!");
 // We have a set of buttons which will change the font-size style property to whatever the textContent of the
 // button is. I'll try to write it in different ways utilizing these concepts
 
-// Grab buttons
-const btns = document.querySelectorAll(".btn");
-btns.forEach(button => {
-    button.addEventListener("click", fontSizer)        
-});
+// Lol there ain't no way this is going to work
 
-function fontSizer(e){
-    let newSize = parseInt(e.target.textContent);
-    document.body.style.fontSize = `${newSize}px`;
-}
+function buttonHandler() {
+    const btns = document.querySelectorAll(".btn");
+    btns.forEach(button => {
+        button.addEventListener("click", function(e) {
+            let newSize = parseInt(e.target.textContent);
+            document.body.style.fontSize = `${newSize}px`;
+        });
+    });
+};
+
+buttonHandler();
