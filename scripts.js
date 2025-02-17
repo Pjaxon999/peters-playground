f2ab7fa5a64e6350e41fe1bcb6782b803887ec23
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-17 10:55:24 -0700

    Add functionality using a closure
    Even though the previous version was clearly superior, I have refactored
    the code to use a closure because I can. I have no idea if I can really
    utilize a factory function for this or if it even makes sense but good
    lord I am going to try my best.

--- scripts.js
+++ scripts.js
@@ -5,16 +5,16 @@ console.log("Hello there!");
 // We have a set of buttons which will change the font-size style property to whatever the textContent of the
 // button is. I'll try to write it in different ways utilizing these concepts
 
-// Lol there ain't no way this is going to work
+function makeSizeChange(size) {
+    return function() {
+        document.body.style.fontSize = `${size}px`;
+    };
+}
 
-function buttonHandler() {
-    const btns = document.querySelectorAll(".btn");
-    btns.forEach(button => {
-        button.addEventListener("click", function(e) {
-            let newSize = parseInt(e.target.textContent);
-            document.body.style.fontSize = `${newSize}px`;
-        });
-    });
-};
+const size14 = makeSizeChange(14);
+const size16 = makeSizeChange(16);
+const size18 = makeSizeChange(18);
 
-buttonHandler();
+document.getElementById("14").onclick = size14;
+document.getElementById("16").onclick = size16;
+document.getElementById("18").onclick = size18; 
